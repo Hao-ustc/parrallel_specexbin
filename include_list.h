42205b6c3b3f826c5ee64b780f087be06073c61f
Hao-ustc <71425713+Hao-ustc@users.noreply.github.com>
2020-12-21 15:03:29 +0800

    Update include_list.h

--- include_list.h
+++ include_list.h
@@ -2,7 +2,7 @@
 #define INCLUDE_LIST_H
 //#include "/data6/Hao_L/code/head/coma/include.h"
 #ifdef _WIN64
-#include "D:\\source\\source_build\\coma\\include.h"
+#include "C:\\git\\sourcebuild\\include.h"
 #endif
 #ifdef _APPLE_
 #include "/Volumes/Samsung USB/source/source_build/coma/include.h"
