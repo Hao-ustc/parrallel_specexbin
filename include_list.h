3225ed69bfce57ddebfca6e80de13fd4f51bc0ea
Hao-ustc <71425713+Hao-ustc@users.noreply.github.com>
2021-03-01 13:58:22 +0800

    Update include_list.h

--- include_list.h
+++ include_list.h
@@ -1,8 +1,30 @@
 #ifndef INCLUDE_LIST_H
 #define INCLUDE_LIST_H
 //#include "/data6/Hao_L/code/head/coma/include.h"
+
+#define SIM_VOIDA
+
+#ifdef SIM_VOIDA
 #ifdef _WIN64
-#include "C:\\git\\sourcebuild\\include.h"
+#include "C:\\git\\sourcebuild\\0a_include_voidA.h"
+#endif
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#include "/Users/Shared/git/sourcebuild/include.h"
+#endif
+
+#ifdef linux
+#include "/data6/Hao_L/code/head/0a_include_voidA.h.h"
+#endif
+#endif
+
+
+
+
+
+#ifdef SIM_COMA
+#ifdef _WIN64
+#include "C:\\git\\sourcebuild\\0a_include_coma.h"
 #endif
 #ifdef __APPLE__
 #include <TargetConditionals.h>
@@ -12,5 +34,5 @@
 #ifdef linux
 #include "/data6/Hao_L/code/head/coma/include.h"
 #endif
-
+#endif
 #endif
