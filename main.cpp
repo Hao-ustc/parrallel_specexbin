cadaded4244b094cb57770d8b3d283906f2b9537
Hao-ustc <71425713+Hao-ustc@users.noreply.github.com>
2021-03-01 16:54:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,10 +13,13 @@ int main(int argc, char *argv[])
     //spece_set.runtime=0;
     int NUM_THREADS = 64;
     int test = -1;
+    int argnumber = 1;
 
     Setting spece_set;
-    sprintf(spece_set.TIME, "%s", argv[1]);
-    sprintf(spece_set.galaxy, "%s", argv[2]);
+    sprintf(spece_set.TIME, "%s", argv[argnumber]);
+    argnumber++;
+    R_assii(argv[argnumber], NUM_THREADS);
+
     sprintf(spece_set.file_redshift, "/data6/Hao_L/result/pure_redshift.txt");
     sprintf(spece_set.spec_ion_filename, "%sspecions_i9.dat", spece_set.prefix);
     spece_set.boxsize = 80; //in Mpc_h
@@ -29,15 +32,10 @@ int main(int argc, char *argv[])
 
     spece_set.phi = -234.0;
 
-    int galaxyposion = 0;
-    int parallelcore = 0;
-    R_assii(argv[2], galaxyposion);
-    R_assii(argv[3], parallelcore);
-
     Los_poss los_poss;
-    R_assii(argv[4], los_poss.los_numbers);
+    R_assii(argv[4], los_poss.los_numbers); //line 的总数
     char los[100];
-    sprintf(los, "./result/%s/parallel/los%03d_%01d.txt", argv[1], galaxyposion, parallelcore);
+    sprintf(los, "./result/%s/parallel/los.txt", argv[1]);
     if ((spece_set.LOSfile = fopen(los, "r")) == NULL)
     {
         cerr << "Could not open file " << los << endl;
@@ -45,7 +43,7 @@ int main(int argc, char *argv[])
     }
     los_poss.in_los = spece_set.LOSfile;
     fclose(spece_set.LOSfile);
-    sprintf(los, "./result/%s/parallel/palace%03d_%01d.txt", argv[1], galaxyposion, parallelcore);
+    sprintf(los, "./result/%s/parallel/palace.txt", argv[1]);
     if ((spece_set.LOSfile = fopen(los, "r")) == NULL)
     {
         cerr << "Could not open file " << los << endl;
@@ -57,10 +55,12 @@ int main(int argc, char *argv[])
     fclose(los_poss.in_los);
     fclose(los_poss.in_palace); //finished
 
+    //line has already been read in
+
     spece_set.load(los_poss.los_pos_vector[0]); //to get filenum
     GASs gass;
     gass.load(spece_set.short_filenum);
-    datafit(gass.void_gas,spece_set,gass.b);
+    datafit(gass.void_gas, spece_set, gass.b);
     vector<GAS> gp;
     gp.clear();
     gp.swap(gass.void_gas);
@@ -68,31 +68,30 @@ int main(int argc, char *argv[])
     sprintf(los, "./result/0_a_width/%s/lines_rp_rper_ews.txt", argv[1]); //output
     ofstream out_ew(los);
 
-    R_assii(argv[5], NUM_THREADS);
     omp_set_num_threads(NUM_THREADS);
 #pragma omp parallel firstprivate(spece_set)
     {
         int thread_id;
         thread_id = omp_get_thread_num();
 
-        for (int losnum = thread_id; losnum < 100; losnum += NUM_THREADS)
+        for (int losnum = thread_id; losnum < los_poss.los_numbers; losnum += NUM_THREADS)
         {
             spece_set.load(los_poss.los_pos_vector[losnum]);
 
             LOS shortlos;
 
             shortlos.shortLOS(spece_set);
-            //cerr << "&&& 1" << endl;
+
             Ion_all spece_ionall;
             spece_ionall.Load(shortlos, spece_set);
-            //cerr << "&&& 2" << endl;
+
             Spec_particles spece_particles;
             spece_particles.Check_Partical_Los(gp, spece_set);
-            //cerr << "&&& 3" << endl;
-            spece_particles.SmoothSpec(shortlos, spece_ionall, spece_set,gass.b);
-            //cerr << "&&& 4" << endl;
+
+            spece_particles.SmoothSpec(shortlos, spece_ionall, spece_set, gass.b);
+
             spece_ionall.Tau(shortlos, spece_set);
-            //cerr << "&&& 5" << endl;
+
             my_EW my_ew;
             int pospoint = OutTau(shortlos, spece_ionall, spece_set, my_ew.redshift_track);
 
@@ -101,6 +100,7 @@ int main(int argc, char *argv[])
 #pragma omp critical
             {
                 out_ew << t_main_lines << " "
+                       << spece_set.galaxyindex_spec << " "
                        << los_poss.los_pos_vector[t_main_lines].rp << " "
                        << los_poss.los_pos_vector[t_main_lines].rp_rvir << " ";
                 for (int ion = 0; ion < my_ew.Nions; ion++)
